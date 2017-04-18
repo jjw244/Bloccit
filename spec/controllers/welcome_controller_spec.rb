a8a169352803a84f9a20d3ca4cde8c139a674b33
Jennifer washburn <jjw244@utexas.edu>
2017-04-18 18:13:44 -0500

    Checkpoint 13 - Rails: Testing - Assignment - COMPLETE

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-# #1 describe the subject of the spec, WelcomeController
+# #1  describe the subject of the spec, WelcomeController
 RSpec.describe WelcomeController, type: :controller do
   describe "GET index" do
     it "renders the index template" do
@@ -15,6 +15,13 @@ RSpec.describe WelcomeController, type: :controller do
     it "renders the about template" do
       get :about
       expect(response).to render_template("about")
+    end
+  end
+
+  describe "GET faq" do
+    it "renders the faq template" do
+      get :faq
+      expect(response).to render_template("faq")
     end
   end
 end
