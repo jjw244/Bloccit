afd76c0d55922703a6cdd509d6b4f9ab75c6efb1
Jennifer washburn <jjw244@utexas.edu>
2017-04-21 06:37:33 -0500

    Checkpoint 17 - Rails: Seeding Data - COMPLETE

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'random_data'
+
+# Create Posts
+ 50.times do
+ # #1  Adding a ! instructs the method to raise an error if there's a problem with the data we're seeding
+   Post.create!(
+ # #2  class RandomData attributes DNE yet -> "Whishful Coding" can increase productivity becasue it allows you to stay focused on one problem at a time
+     title:  RandomData.random_sentence,
+     body:   RandomData.random_paragraph
+   )
+ end
+ posts = Post.all
+
+ # Create Comments
+ # #3  x.times runs a block x times
+ 100.times do
+   Comment.create!(
+ # #4  call sample on Post.all array inorder to pick a random post to associate each comment with; returns a random element from the array every time it's called
+     post: posts.sample,
+     body: RandomData.random_paragraph
+   )
+ end
+
+ puts "Seed finished"
+ puts "#{Post.count} posts created"
+ puts "#{Comment.count} comments created"
---
+++ lib/random_data.rb
@@ -0,0 +1,31 @@
+# #5 RandomData is a standalone library with no dependencies or inheritance requirements
+ module RandomData
+ # #6  random_paragraph -> create 4-6 random sentences and append them to the sentences array
+   def self.random_paragraph
+     sentences = []
+     rand(4..6).times do
+       sentences << random_sentence
+     end
+
+     sentences.join(" ")
+   end
+
+ # #7  random_sentence -> create 3-8 random words and append them to the strings array
+   def self.random_sentence
+     strings = []
+     rand(3..8).times do
+       strings << random_word
+     end
+
+     sentence = strings.join(" ")
+     sentence.capitalize << "."
+   end
+
+ # #8  random_word -> set a-z to an array, shuffle the array in place, join 0th-nth letters
+  #nth -> rand(3..8) returns a random number <= 3 and > 8
+   def self.random_word
+     letters = ('a'..'z').to_a
+     letters.shuffle!
+     letters[0,rand(3..8)].join
+   end
+ end
