ba27e0c86ea27e573f29de4562d60265f83b1253
Jennifer washburn <jjw244@utexas.edu>
2017-04-28 18:47:57 -0500

    Checkpoint 19 - Rails: CRUD - COMPLETE

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,12 +2,41 @@ class PostsController < ApplicationController
   def index
 # #11 declare an instance variable and assign it a collection of Post objects using the all method provided by ActiveRecord
     @posts = Post.all
+
+    @posts.all.each_with_index do |post, index|
+      if index % 5 == 0
+        post.title = "SPAM"
+      end
+    end
   end
 
   def show
+# #19  find the post that corresponds to the id in the params that was passed to  show and assign it to @post
+  # Unlike in the index method, in the show method, we populate an instance variable with a single post, rather than a collection of posts
+    @post = Post.find(params[:id])
   end
 
   def new
+  # #7  create an instance variable, @post, then assign it an empty post returned by Post.new
+    @post = Post.new
+  end
+
+  def create
+# #9  call Post.new to create a new instance of Post
+    @post = Post.new
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+
+# #10 if Post is successfully saved (see #11); Redirecting to @post will direct the user to the posts show view
+    if @post.save
+# #11  assign a value to flash[:notice]. The flash hash provides a way to pass temporary values between actions. Any value placed in flash will be available in the next action and then deleted
+      flash[:notice] = "Post was saved."
+      redirect_to @post
+    else
+# #12  if not, we display an error message and render the new view again
+      flash.now[:alert] = "There was an error saving the post. Please try again."
+      render :new
+    end
   end
 
   def edit
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe PostsController, type: :controller do
 
   describe "GET #index" do
     it "returns http success" do
-  # #7 the test performs a GET on the index view and expects the ersponse to be successful
+  # #7 the test performs a GET on the index view and expects the response to be successful
       get :index
       expect(response).to have_http_status(:success)
     end
@@ -23,25 +23,59 @@ RSpec.describe PostsController, type: :controller do
     end
   end
 
-# #10  we will use these later
-  # describe "GET #show" do
-  #   it "returns http success" do
-  #     get :show
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
-  #
-  # describe "GET #new" do
-  #   it "returns http success" do
-  #     get :new
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
-  #
-  # describe "GET #edit" do
-  #   it "returns http success" do
-  #     get :edit
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+# #1  a new and unsaved Post object is created
+  describe "GET new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+# #2  expect PostsController#new to render the psts new view, and use render_template method to verify that the correct template(view) is rendered.
+    it "renders the #new view" do
+      get :new
+      expect(response).to render_template :new
+    end
+
+# #3  expect the @post instance variable to be initialized by  PostsController#new. assigns gives us access to the @post variable, assigning it to :post.
+    it "instantiates @post" do
+      get :new
+      expect(assigns(:post)).not_to be_nil
+    end
+  end
+
+  describe "POST create" do
+# #4  the newly created object is persisted to the database
+  #  expect that after PostsController#create is called with the parameters  {post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}}, the count of Post instances (i.e. rows in the posts table) in the database will increase by one
+    it "increases the number of Post by 1" do
+      expect{post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}}.to change(Post,:count).by(1)
+    end
+
+# #5  when create is POSTed to, we expect the newly created post to be assigned to @post.
+    it "assigns the new post to @post" do
+      post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
+    end
+
+# #6  we expect to be redirected to the newly created post
+    it "redirects to the new post" do
+      post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
+    end
+  end
+
+  describe "GET show" do
+    it "returns http success" do
+# #16  pass {id: my_post.id} to show as a parameter. These parameters are passed to the  params hash
+      get :show, {id: my_post.id}
+      expect(response).to have_http_status(:success)
+    end
+    it "renders the #show view" do
+# #17  expect the response to return the show view using the  render_template matcher
+      get :show, {id: my_post.id}
+      expect(response).to render_template :show
+    end
+
+    it "assigns my_post to @post" do
+      get :show, {id: my_post.id}
+# #18  expect the post to equal my_post because we call show with the id of  my_post
+      expect(assigns(:post)).to eq(my_post)
+    end
+  end
 end
