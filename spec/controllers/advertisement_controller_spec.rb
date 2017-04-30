4e94337e4308df5fe3a1a6b40adf25c3368f246f
Jennifer washburn <jjw244@utexas.edu>
2017-04-30 11:55:01 -0500

    Checkpoint 19 - Rails: CRUD - Assignment - COMPLETE

---
+++ spec/controllers/advertisement_controller_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+# include RandomData
+
+RSpec.describe AdvertisementController, type: :controller do
+
+  let(:my_advertisement) { Advertisement.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph)}
+
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+
+    it "assigns [my_advertisement] to @advertisement" do
+      get :index
+      expect(assigns(:advertisement)).to eq([my_advertisement])
+    end
+  end
+
+  describe "GET show" do
+    it "returns http success" do
+      get :show, {id: my_advertisement.id}
+      expect(response).to have_http_status(:success)
+    end
+    it "renders the #show view" do
+      get :show, {id: my_advertisement.id}
+      expect(response).to render_template :show
+    end
+
+    it "assigns my_advertisement to @post" do
+      get :show, {id: my_advertisement.id}
+      expect(assigns(:advertisement)).to eq(my_advertisement)
+    end
+  end
+
+  describe "GET #new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "ADVERTISEMENT create" do
+    it "increases the number of Advertisement by 1" do
+      expect{post :create, advertisement: {title: RandomData.random_sentence, body: RandomData.random_paragraph}}.to change(Advertisement,:count).by(1)
+    end
+
+    it "assigns the new advertisement to @advertisement" do
+      post :create, advertisement: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
+    end
+
+    it "redirects to the new post" do
+      post :create, advertisement: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
+    end
+  end
+
+end
