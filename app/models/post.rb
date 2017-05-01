f87d6ae39259c968a3ab1e97668ab4aa05e57fdb
Jennifer washburn <jjw244@utexas.edu>
2017-04-30 21:01:55 -0500

    Checkpoint 20 - Rails: More CRUD - COMPLETE

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
 # has_many method allows a post instance to have many commetns related to it,
- # and also provides methods that allow us to retrieve somments that belong to a post 
-  has_many :comments
+ # and also provides methods that allow us to retrieve somments that belong to a post
+  has_many :comments, dependent: :destroy
 end
